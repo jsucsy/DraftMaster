1131e26d3aa9a4b6e2d918929749ae1e636c15d5
jsu <jsu@falconfone.com>
2013-07-30 18:14:06 -0400

    Added config, skeleton GUI
    Signed-off-by: jsu <jsu@falconfone.com>

---
+++ root/Config.py
@@ -0,0 +1,8 @@
+'''
+Created on Jul 30, 2013
+
+@author: JSU
+'''
+
+is_snake = True
+num_rounds = 16
--- root/GuiMain.py
+++ root/GuiMain.py
@@ -4,5 +4,64 @@ Created on Jul 30, 2013
 @author: JSU
 '''
 from Tkinter import *
+import Config as cfg
 
-root = Tk()
+TEAMS = {}
+PLAYERS = {}
+DRAFT_ORDER = []
+
+
+
+def btn_pullteams_call():
+    pullteams("%s%s" % (REFDIR, "teams_test.txt"))
+    for team in DRAFT_ORDER:
+        print TEAMS[team][0]
+
+def btn_startdraft_call():
+    draftlive = Toplevel()
+    draftlive.title("Drafting")
+    
+    buttons = []
+    btn_finishdraft = Button(draftlive, text = "Finish Draft", command = draftlive.destroy())
+    buttons.append(btn_finishdraft)
+    
+    [x.pack() for x in buttons]
+    
+
+
+
+def pullteams(sourcefile):
+    with open(sourcefile, 'rb+') as source:
+        team_order = []
+        for line in source:
+            fields = line.split('|')
+            team_order.append(fields[0])
+            TEAMS[fields[0]] = fields[1:-1]
+        if cfg.is_snake:
+            print team_order
+            for x in range(cfg.num_rounds):
+                if x % 2 == 1:
+                    for team in reversed(team_order):
+                        DRAFT_ORDER.append(team)
+                else:
+                    for team in team_order:
+                        DRAFT_ORDER.append(team)
+
+def runframe():
+    buttons = []
+    btn_pullteams = Button(MASTER, text = "Pull Teams", command = btn_pullteams_call)
+    buttons.append(btn_pullteams)    
+    btn_startdraft = Button(MASTER, text = "Start Draft", command = btn_startdraft_call)
+    buttons.append(btn_startdraft)
+    
+    [x.pack() for x in buttons]
+    
+    mainloop()
+
+if __name__ == '__main__':
+    MASTER = Tk()
+    REFDIR = "C:\\dropbox\\football\\draftmaster\\"
+    
+    runframe()
+
+

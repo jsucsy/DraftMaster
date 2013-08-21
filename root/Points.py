4c64dc08d431c2788a2e39044a54659ac6835ba1
jsu <jsu@falconfone.com>
2013-08-21 17:08:25 -0400

    comments added to points
    Signed-off-by: jsu <jsu@falconfone.com>

--- root/Points.py
+++ root/Points.py
@@ -14,3 +14,11 @@ class Points:
     
 if __name__ == '__main__':
     print Points.i
+    
+    
+    
+'''higher than expected.[11] In the football articles, he names the players he likes to exceed their expectations for that week, and the guys who he expects to do worse than the fantasy benchmarks for their respective positions. The fantasy benchmarks are as follows:
+Quarterbacks - 175 yards and two touchdowns (15 fantasy points in ESPN standard scoring)
+Running backs and Wide Receivers - 90 yards OR 30 yards and a touchdown rushing or receiving (9 fantasy points)
+Tight ends - 60 yards or a touchdown (6 fantasy points)
+Defenses - 8 fantasy points[12]'''
